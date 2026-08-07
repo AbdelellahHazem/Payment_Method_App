79f1c3acd87d04978e70d5a060ab9e330bb99ad5
AbdelellahHazem <141696492+AbdelellahHazem@users.noreply.github.com>
2026-08-07 22:27:43 +0300

    app styles is created

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const TextStyle textStyle25 = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+
+ static const TextStyle textStyle18 = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+
+
+  static const TextStyle textStyle1Bold8 = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+
+
+   static const TextStyle textStyle24 = TextStyle(
+    fontSize: 24,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+
+
+   static const TextStyle textStyle20 = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Inter',
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  
+}
--- lib/features/checkout/presentation/views/my_cartview.dart
+++ lib/features/checkout/presentation/views/my_cartview.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:payment_method_app/core/utils/styles.dart';
 
 class MyCartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('My Cart')));
+    return Scaffold(appBar: AppBar(title: Text('My Cart',style: Styles.textStyle25,),));
   }
 }
